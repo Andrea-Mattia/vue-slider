73c65baf3bc7b83d40ef21d1d41dc9c8f9e67659
Andrea-Mattia <andrea.mattia95@gmail.com>
2021-04-21 16:59:24 +0200

    add navigation by arrows

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@
  * Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
  */
 
+ const slider = new Vue({
+    el: '#app',
+    data: {
+        images: [
+            './img/img1.jpg',
+            './img/img2.jpg',
+            './img/img3.jpg',
+            './img/img4.jpg',
+        ],
+        imgIndex: 0,
+    },
+    methods: {
+        prevImg() {
+            this.imgIndex--;
+            if (this.imgIndex < 0) {
+                this.imgIndex = this.images.length - 1;
+            }
+        },
+        nextImg() {
+            this.imgIndex++;
+            if (this.imgIndex > this.images.length - 1) {
+                this.imgIndex = 0;
+            }
+        },
+    },
+});
